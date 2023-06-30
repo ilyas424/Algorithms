edc9a8d558d61b8817939fd5361ad153a4a2b576
ilyas424 <ilyasich123@gmail.com>
2023-06-30 13:02:00 +0300

    new tests

--- InsertionSort.py
+++ InsertionSort.py
@@ -13,9 +13,11 @@ def insertion_sort(lst: list)-> list:
 def test():
 
     for _ in range(10000):
-        data = [randint(-1000, 1000) for _ in range(30)]
+        data = [randint(-1000, 1000) for _ in range(10)]
         assert sorted(data) == insertion_sort(data)
-
+    assert [1]  == insertion_sort([1])
+    assert [] == insertion_sort([])
+    assert [0.2, 1.5, 2.1, 2.5] == insertion_sort([1.5, 0.2, 2.5, 2.1])
     print(f'😊 tests successful')
 
 
